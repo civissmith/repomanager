ebda52f63926b0abfd1e4a2360a10888ae09e7c5
Phil Smith <civis.smith@gmail.com>
2015-01-08 04:36:26 -0700

    Worked login code
    Added temporary users and started working logic to allow logins.
    Repo and landing pages are stubbed.

--- app.py
+++ app.py
@@ -42,36 +42,79 @@ app = fl.Flask(__name__)
 # Set the temporary secret key
 app.secret_key = "FOO"
 
+user_db = {'superman':'kent',
+           'batman'  :'wayne',
+          }
+
 class Landing( fv.MethodView ):
   """
   The Landing view is responsible for handling the splash page or site
   landing page.
   """
   def get(self):
+    # Temporarily just re-direct. This will need to change once sessions
+    # are established.
+    return fl.redirect(fl.url_for('auth'))
     return fl.render_template('landing.html')
 
   def post(self):
-    pass
+    return "<h1>Landing Page: POST</h1>"
 
 class Auth( fv.MethodView ):
   """
   The Auth view is responsible for handling user authentication (login/logout).
   """
   def get(self):
-    return "Auth"
+     
+    return fl.render_template('auth.html')
 
   def post(self):
-    pass
 
+    # Check to see if the request is a logout.
+    if 'logout' in fl.request.form:
+      print "logout"
+      fl.session.pop('username', None)
+      return fl.redirect(fl.url_for('auth'))
+  
+
+    # List the required credentials
+    required = ['username', 'password']
+
+    # Check each credential in the request object
+    for cred in required:
+
+      # If either username or password are missing, flash a message then 
+      # return to the login page.
+      # NOTE: This is not the check for empty strings (those are handled by the
+      #       validation check)
+      if cred not in fl.request.form:
+        fl.flash("%s is required." % cred)
+        return fl.redirect( fl.url_for('auth'))
+
+    creds = self.validate_creds( fl.request.form['username'],
+                                 fl.request.form['password'])
+    if creds:
+      return fl.redirect(fl.url_for('repo'))
+    else:
+      fl.flash('Invalid username or password!')
+      return fl.redirect( fl.url_for('auth'))
+
+  def validate_creds(self, uname, passwd):
+    # Validate the username and password.
+    # NOTE: Do not rely on the Javascript layer to authenticate.
+    if uname in user_db and user_db[uname] == passwd:
+      fl.session['username'] = uname
+      return True    
+    return False
 
 class Repo( fv.MethodView ):
   """
   The Repo view is responsible for handling the repository management functions.
   """
   def get(self):
-    return "Repo"
+    return "<h1>Repo Manager: GET</h1>"
   def post(self):
-    pass
+    return "<h1>Repo Manager: POST</h1>"
 
 
 # Add URL rules so the app can navigate.
